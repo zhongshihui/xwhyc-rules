1819a922cd65aad769ea19123cd3814392da5106
adbyby <ilolitata@qq.com>
2017-10-14 09:15:40 +0800

    new rules

---
+++ update3.js
@@ -0,0 +1,11 @@
+;更新规则url,dir,size,reverse,reverse
+!更新规则url,dir,size,reverse,reverse
+http://update.adbyby.com/rule3/lazy.jpg	\data\lazy.txt	484274	4	0	
+http://update.adbyby.com/rule3/video.jpg	\data\video.txt	779	4	0	
+http://update.adbyby.com/rule3/user.action	\user.action	459	5	0	
+!https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/clean.ini	\data\clean.ini	2167	0	0	
+!https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/adclear.ini	\data\adclear.ini	4901	0	0	
+!https://github.com/gchangchen/suho/raw/master/adhook.dll	\adhook.dll	117248	0	0	2.4.3.2
+!https://github.com/gchangchen/suho/raw/master/adhook64.dll	\adhook64.dll	142848	0	0	2.4.3.2
+!https://github.com/gchangchen/suho/raw/master/adbyby.exe	\adbyby.exe	1004183	1	0	2.4.4.0
+
