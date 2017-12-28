d0b347956405c3296652ee99d2f0cdc24a23dd38
adbyby <1822805448@qq.com>
2017-12-28 20:47:52 +0800

    updated rules

--- update3.js
+++ update3.js
@@ -1,11 +1,10 @@
-﻿;¸üÐÂ¹æÔòurl,dir,size,reverse,reverse
+;¸üÐÂ¹æÔòurl,dir,size,reverse,reverse
 !¸üÐÂ¹æÔòurl,dir,size,reverse,reverse
-https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt	\data\lazy.txt	493789	4	0	
-https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt	\data\video.txt	779	4	0	
+https://coding.net/u/adbyby/p/xwhyc-rules/git/raw/master/lazy.txt	\data\lazy.txt	493984	4	0	
+https://coding.net/u/adbyby/p/xwhyc-rules/git/raw/master/video.txt	\data\video.txt	779	4	0	
 http://update.adbyby.com/rule3/user.action	\user.action	459	5	0	
 !https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/clean.ini	\data\clean.ini	2167	0	0	
 !https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/adclear.ini	\data\adclear.ini	4901	0	0	
 !https://github.com/gchangchen/suho/raw/master/adhook.dll	\adhook.dll	117248	0	0	2.4.3.2
 !https://github.com/gchangchen/suho/raw/master/adhook64.dll	\adhook64.dll	142848	0	0	2.4.3.2
-!https://github.com/gchangchen/suho/raw/master/adbyby.exe	\adbyby.exe	1004183	1	0	2.4.4.0
-
+!https://github.com/gchangchen/suho/raw/master/adbyby.exe	\adbyby.exe	1004183	1	0	2.4.4.
