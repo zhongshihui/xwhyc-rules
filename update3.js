09b443eecebd9917937a98c30aeb336fc46ea59d
adbyby <1822805448@qq.com>
2018-03-03 08:56:24 +0800

    updated rules

--- update3.js
+++ update3.js
@@ -4,7 +4,7 @@ https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt	\data\lazy.
 https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt	\data\video.txt	4918	4	0	
 http://update.adbyby.com/rule3/user.action	\user.action	459	5	0	
 !https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/clean.ini	\data\clean.ini	2167	0	0	
-!https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/adclear.ini	\data\adclear.ini	447	0	0	
+!https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/adclear.ini	\data\adclear.ini	4901	0	0	
 !https://github.com/gchangchen/suho/raw/master/adhook.dll	\adhook.dll	117248	0	0	2.4.3.2
 !https://github.com/gchangchen/suho/raw/master/adhook64.dll	\adhook64.dll	142848	0	0	2.4.3.2
 !https://github.com/gchangchen/suho/raw/master/adbyby.exe	\adbyby.exe	1004183	1	0	2.4.4.
