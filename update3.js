7af17ebb7b360f3e21f5a715229bddddebc74b32
adbyby <ilolitata@qq.com>
2018-04-10 08:04:26 +0800

    update rules

--- update3.js
+++ update3.js
@@ -1,10 +1,10 @@
 ;¸üÐÂ¹æÔòurl,dir,size,reverse,reverse
 !¸üÐÂ¹æÔòurl,dir,size,reverse,reverse
 https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt	\data\lazy.txt	511824	4	0	
-https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt	\data\video.txt 837	4	0	
+https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt	\data\video.txt 1119	4	0	
 http://update.adbyby.com/rule3/user.action	\user.action	459	5	0	
 !https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/clean.ini	\data\clean.ini	2167	0	0	
 !https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/adclear.ini	\data\adclear.ini	4901	0	0	
 !https://github.com/gchangchen/suho/raw/master/adhook.dll	\adhook.dll	117248	0	0	2.4.3.2
 !https://github.com/gchangchen/suho/raw/master/adhook64.dll	\adhook64.dll	142848	0	0	2.4.3.2
-!https://github.com/gchangchen/suho/raw/master/adbyby.exe	\adbyby.exe	1004183	1	0	2.4.4.
+!https://github.com/gchangchen/suho/raw/master/adbyby.exe	\adbyby.exe	1004183	1	0	2.4.4.0
